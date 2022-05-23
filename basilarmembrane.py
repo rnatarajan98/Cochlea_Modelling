44a1177b2e7e463f13b9d708d6474fd46d7a5cbd
rnatarajan98 <richard.natarajan.98@gmail.com>
2022-05-23 14:54:46 +0100

    Created gammatone class, intended to allow multiple filters to be called identically

--- basilarmembrane.py
+++ basilarmembrane.py
@@ -1,6 +1,18 @@
 from scipy import signal
 import numpy as np
 
+class gammatone:
+    def __init__(self, f_center, fs):
+        #Models a 4th order gammatone filter with an 8th order IIR
+        #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.gammatone.html#rf19e61802808-1
+        self.center = f_center
+        self.fs = fs
+        self.b, self.a = signal.gammatone(f_center, ftype='iir', fs=fs)
+        
+    def filter(self, input):
+        sig_filt = signal.lfilter(self.b, self.a, output)
+        return sig_filt
+        
 
 #Glasberg and Moore, 1990)
 def f2erb(f):
@@ -11,13 +23,6 @@ def erb2f(erb):
     f = 676170.4/(47.06538 - np.exp(0.08950404*erb)) - 14678.49
     return f
 
-
-def gammatone(f_center, fs):
-    #Models a 4th order gammatone filter with an 8th order IIR
-    #https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.gammatone.html#rf19e61802808-1
-    filter = signal.gammatone(f_center, ftype='iir', fs=fs)
-    return filter
-
 def gammatone_freqs(flims, nfilt):
     erblims = [f2erb(flims[0]), f2erb(flims[1])]
     erbs = np.linspace(erblims[0], erblims[1], nfilt)
@@ -25,7 +30,7 @@ def gammatone_freqs(flims, nfilt):
     return filts
 
 def gammatone_filterbank(flims, nfilt, fs):
-    filt_locs = basilarmembrane.gammatone_freqs(flims, nfilt)
+    filt_locs = gammatone_freqs(flims, nfilt)
     bands = {str(f): {"filter":gammatone(f, fs)} for f in filt_locs}
     filterbank = {"type":"gammatone", "sampling_frequency":fs, "bands":bands}
     return filterbank
